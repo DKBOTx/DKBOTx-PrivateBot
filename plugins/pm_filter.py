43f1a4d1bcf94af9032b35f0b814292e2c2e9abe
DK BOTx TG <100774327+DKBOTx@users.noreply.github.com>
2022-04-01 20:03:33 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -99,8 +99,9 @@ async def next_page(bot, query):
     elif off_set is None:
         btn.append(
             [InlineKeyboardButton(f"🗓 {round(int(offset) / 10) + 1} / {round(total / 10)}", callback_data="pages"),
-             InlineKeyboardButton("NEXT ⏩", callback_data=f"next_{req}_{key}_{n_offset}")])
+             InlineKeyboardButton("NEXT ⏩", callback_data=f"next_{req}_{key}_{n_offset}")]
             [InlineKeyboardButton('🔒 Close', callback_data='close_data')]
+        )
     else:
         btn.append(
             [
